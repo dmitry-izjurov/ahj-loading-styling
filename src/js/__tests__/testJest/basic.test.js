01cf9cd5f8e7df53178517d3ec919ef1b949dee7
Dmitry <dmitry.izjurov@yandex.ru>
2022-08-18 18:18:17 +0300

    Добавлен тест

--- src/js/__tests__/testJest/basic.test.js
+++ src/js/__tests__/testJest/basic.test.js
@@ -1,31 +1,6 @@
-import { getCoorStr } from '../../utils';
+import Inspector from '../../Inspector';
 
-test('Объект должен возращать координаты', () => {
-  const result = getCoorStr('51.50851,-0.12572');
-  expect(result).toBe('51.50851,-0.12572');
-});
-
-test('Объект должен возращать координаты', () => {
-  const result = getCoorStr(' 51.50851  ,  -0.12572   ');
-  expect(result).toBe('51.50851,-0.12572');
-});
-
-test('Объект не должен возращать координаты', () => {
-  const result = getCoorStr('51.50851,-0.12572ь');
-  expect(result).toBe(undefined);
-});
-
-test('Объект не должен возращать координаты', () => {
-  const result = getCoorStr('Координаты');
-  expect(result).toBe(undefined);
-});
-
-test('Объект не должен возращать координаты', () => {
-  const result = getCoorStr('51.50851');
-  expect(result).toBe(undefined);
-});
-
-test('Объект не должен возращать координаты', () => {
-  const result = getCoorStr('51.50851, 51.50851, -0.12572');
-  expect(result).toBe(undefined);
+test('Объект должен создаваться', () => {
+  const unit = new Inspector();
+  expect(unit).toEqual({ data: undefined });
 });
